1c4ac602d934663739fa2a84ab28270eaaab630c
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-05-23 19:41:56 +0100

    Debug typos

--- recop.it/src/scripts/ingest/get_page.py
+++ recop.it/src/scripts/ingest/get_page.py
@@ -1,11 +1,11 @@
 import os
-import loging
+import logging
 import requests
 import pyperclip
 from pathlib import Path
 
-logging.basicConfig(format = "%(asctime)s [%(levelname)] \
-							  %(funcName): %(message)s",
+logging.basicConfig(format="%(asctime)s [%(levelname)s] \
+							%(funcName)s: %(message)s",
 					level =  logging.DEBUG)
 logger = logging.getLogger()
 
@@ -13,13 +13,13 @@ def get_webpage():
 	"""Function to download the page from the internet"""
 
 	# This function copies the webpage from my clipboard
-	WEB_PAGE = pyperclip.paste()
+	page_address = pyperclip.paste()
 	try:
-		page = requests.get(WEB_PAGE)
-		logger.info("%(WEB_PAGE)s successfully retrieved")
+		page = requests.get(page_address)
+		logger.info(f"{page_address} successfully retrieved")
 	except HTTPError:
-		logger.exception(" The webpae does not exist")
-	return WEB_PAGE
+		logger.exception(" The webpage does not exist")
+	return page
 
 def save_page(WEB_PAGE):
 	"""This function creates a document for the downloaded webpage"""
@@ -30,7 +30,7 @@ def save_page(WEB_PAGE):
 		# Iterating through the data downloaded
 		for page in WEB_PAGE.iter_content(DATA_SIZE):
 			file.write(page.decode())
-		logger.info("%(FILE_NAME) saved successfully")
+		logger.info(f"{FILE_NAME} saved successfully")
 	return None
 
 def get_filename():
@@ -38,11 +38,11 @@ def get_filename():
 	present_location = os.getcwd()
 	DATA_FOLDER = "Documents/Programming/recop.it/recop.it/data/raw"
 	final_location = None
-	if present_location = Path(Path.home, DATA_FOLDER):
+	if present_location == Path(Path.home(), DATA_FOLDER):
 		final_location = present_location
 	else:
-		final_location = Path(Path.home, DATA_FOLDER)
-		logger.info("Changed file destination to %(final_location)s")
+		final_location = Path(Path.home(), DATA_FOLDER)
+		logger.info(f"Changed file destination to {final_location}")
 
 	FILE_NAME = f"{str(final_location)}/webpage.txt"
 	FILE_NAME = verify_filename(FILE_NAME)
@@ -56,14 +56,14 @@ def verify_filename(FILE_NAME):
 	file_exists = os.path.exists(FILE_NAME)
 
 	while file_exists:
-		logger.info("%(FILE_NAME)s exist if folder, renaming.......")
-		basename = os.path.basename().split(".")
-		dir_name = os.path.dirname()
+		logger.info(f"{FILE_NAME} exist if folder, renaming.......")
+		basename = os.path.basename(FILE_NAME).split(".")
+		dir_name = os.path.dirname(FILE_NAME)
 		FILE_NAME = f"{str(dir_name)}/{basename[0]}{COUNT}.{basename[1]}"
 		COUNT += 1
 		file_exists = os.path.exists(FILE_NAME)
 
-	logger.info("File renamed as %(FILE_NAME)s")
+	logger.info(f"File renamed as {FILE_NAME}")
 	return FILE_NAME
 
 def main():
