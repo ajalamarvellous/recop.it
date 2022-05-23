f14c891d9f7ac5dd7f03d268f52ad377266228c5
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-05-23 14:47:22 +0100

    Add log configuration

--- recop.it/src/scripts/ingest/get_page.py
+++ recop.it/src/scripts/ingest/get_page.py
@@ -2,3 +2,7 @@ import os
 import loging
 import requests
 import pyperclip
+
+logging.basicConfig(format = "%(asctime)s [%(levelname)] \
+							  %(funcName): %(message)s",
+					level =  logging.DEBUG)
