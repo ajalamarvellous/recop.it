7f15e827ed9f80f50ff0ba6c42c5473c1709146f
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-05-23 16:05:01 +0100

    Add function 'get_filename' to get the filename

--- recop.it/src/scripts/ingest/get_page.py
+++ recop.it/src/scripts/ingest/get_page.py
@@ -2,6 +2,7 @@ import os
 import loging
 import requests
 import pyperclip
+from pathlib import Path
 
 logging.basicConfig(format = "%(asctime)s [%(levelname)] \
 							  %(funcName): %(message)s",
@@ -24,9 +25,22 @@ def save_page(WEB_PAGE):
 	"""This function creates a document for the downloaded webpage"""
 
 	DATA_SIZE = 5024
-	FILE_NAME =
+	FILE_NAME = get_filename()
 	with open(FILE_NAME, "w") as file:
-		# Iteracting through the data downloaded
+		# Iterating through the data downloaded
 		for page in WEB_PAGE.iter_content(DATA_SIZE):
 			file.write(page.decode())
 	return None
+
+def get_filename():
+	"""This function gets the location in which the file will be stored"""
+	present_location = os.getcwd()
+	DATA_FOLDER = "Documents/Programming/recop.it/recop.it/data/raw"
+	final_location = None
+	if present_location = Path(Path.home, DATA_FOLDER):
+		final_location = present_location
+	else:
+		final_location = Path(Path.home, DATA_FOLDER)
+
+	FILE_NAME = f"{str(final_location)}/webpage.txt"
+	return FILE_NAME
