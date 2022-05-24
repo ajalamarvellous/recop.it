e702db5966156176319e3aab9d0cb30e91ec719c
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-05-24 11:46:46 +0100

    Add function to get .txt data files

--- recop.it/src/scripts/ingest/parse_page.py
+++ recop.it/src/scripts/ingest/parse_page.py
@@ -1,3 +1,18 @@
+import os
 import logging
 from pathlib import Path
 from bs4 import BeautifulSoup
+
+logging.basicConfig(format="%(asctime)s [%(levelname)] \
+							%(funcName)s: %(message)s",
+					level = logging.DEBUG)
+logger = logging.getLogger()
+
+# Location to the saved txt files
+LOCATION = Path(Path.home(),
+				"Documents/Programming/recop.it/recop.it/data/raw")
+
+def get_files():
+	"""Function to get the .txt data files"""
+	files = list(LOCATION.glob("*.txt"))
+	return files
