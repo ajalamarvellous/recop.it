52c2f51316f42aa6c4449876825aa9a70dc06e84
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-05-25 12:11:36 +0100

    Add 'parse()' function to parse the file and obtain needed data
    This data collected include Title of the product, rating, brand, Product details, specifications

--- recop.it/src/scripts/ingest/parse_page.py
+++ recop.it/src/scripts/ingest/parse_page.py
@@ -2,6 +2,7 @@ import os
 import logging
 from pathlib import Path
 from bs4 import BeautifulSoup
+from lxml import html
 
 logging.basicConfig(format="%(asctime)s [%(levelname)] \
 							%(funcName)s: %(message)s",
@@ -17,12 +18,42 @@ def get_files():
 	files = list(LOCATION.glob("*.txt"))
 	return files
 
+def parse(doc):
+	"""
+	This function parse the file and extract the needed information from the
+	the file
+	"""
+	main_section = doc.xpath("//main[@class='-pvs']")[0]
+	title = main_section.xpath(
+								'//div[@class="-fs0 -pls -prl"]'
+								)[0].text_content()
+	rating = main_section.xpath(
+								'//div[@class="stars _s _al"]'
+								)[0].text_content()
+	brand = main_section.xpath('//div[@class="-pvxs"]')[0].text_content()
+	price = main_section.xpath(
+								'//div[@class="-hr -mtxs -pvs"]'
+								)[0].text_content()
+	product_details = main_section.xpath(
+							'//div[@class="markup -mhm -pvl -oxa -sc"]'
+										)[0].text_content()
+	spec = main_section.xpath('//div[@class="markup -pam"]')[0].text_content()
+	print(f"Title: {title}\n\
+			Price: {price}\n\
+			Rating: {rating}\n\
+			Brand: {brand}\n\
+			Product Details: {product_details}\n\
+			Specifications: {spec}")
+
+
+
 def main():
 	files = get_files()
 	for file in files:
-		with open(file, "r") as doc:
-			soup_doc = BeautifulSoup(doc,"html.parser")
-			
+		doc = html.parse(file)
+		parse(doc)
+		print("*"*100)
+
 
 if __name__ == "__main__":
 	main()
