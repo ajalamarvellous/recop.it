657981dd6d1e13ee258ab4b32f38948fc2b6c55c
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-05-25 13:02:56 +0100

    Comment properly

--- recop.it/src/scripts/ingest/get_page.py
+++ recop.it/src/scripts/ingest/get_page.py
@@ -37,41 +37,40 @@ def save_page(WEB_PAGE):
     logger.info(f"{file.name} saved successfully")
     return file
 
-"""
-def get_filename():
-    This function gets the location in which the file will be stored
-    present_location = os.getcwd()
-    DATA_FOLDER = "Documents/Programming/recop.it/recop.it/data/raw"
-    final_location = None
-    if present_location == Path(Path.home(), DATA_FOLDER):
-        final_location = present_location
-    else:
-        final_location = Path(Path.home(), DATA_FOLDER)
-        logger.info(f"Changed file destination to {final_location}")
 
-    FILE_NAME = f"{str(final_location)}/webpage.txt"
-    FILE_NAME = verify_filename(FILE_NAME)
-    return FILE_NAME
+#def get_filename():
+#    This function gets the location in which the file will be stored
+#    present_location = os.getcwd()
+#    DATA_FOLDER = "Documents/Programming/recop.it/recop.it/data/raw"
+#    final_location = None
+#    if present_location == Path(Path.home(), DATA_FOLDER):
+#        final_location = present_location
+#    else:
+#        final_location = Path(Path.home(), DATA_FOLDER)
+#        logger.info(f"Changed file destination to {final_location}")
+
+#    FILE_NAME = f"{str(final_location)}/webpage.txt"
+#    FILE_NAME = verify_filename(FILE_NAME)
+#    return FILE_NAME
 
 
-def verify_filename(FILE_NAME):
-    This function verifies if there is an existing filename
-    and returns another name if the name exists
-    COUNT = 0
-    FILE_NAME = FILE_NAME
-    file_exists = os.path.exists(FILE_NAME)
+#def verify_filename(FILE_NAME):
+#    This function verifies if there is an existing filename
+#    and returns another name if the name exists
+#    COUNT = 0
+#    FILE_NAME = FILE_NAME
+#    file_exists = os.path.exists(FILE_NAME)
 
-    while file_exists:
-        logger.info(f"{FILE_NAME} exist if folder, renaming.......")
-        basename = os.path.basename(FILE_NAME).split(".")
-        dir_name = os.path.dirname(FILE_NAME)
-        FILE_NAME = f"{str(dir_name)}/{basename[0]}{COUNT}.{basename[1]}"
-        COUNT += 1
-        file_exists = os.path.exists(FILE_NAME)
+#    while file_exists:
+#        logger.info(f"{FILE_NAME} exist if folder, renaming.......")
+#        basename = os.path.basename(FILE_NAME).split(".")
+#        dir_name = os.path.dirname(FILE_NAME)
+#        FILE_NAME = f"{str(dir_name)}/{basename[0]}{COUNT}.{basename[1]}"
+#        COUNT += 1
+#        file_exists = os.path.exists(FILE_NAME)
 
-    logger.info(f"File renamed as {FILE_NAME}")
-    return FILE_NAME
-"""
+##    return FILE_NAME
+
 
 def main():
     WEB_PAGE = get_webpage()
