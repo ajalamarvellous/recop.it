a6b88e62c9eb2205a4c167cddb38919151850d65
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-07-15 11:32:42 +0100

    Add stack_df to stack dataframes on top of one another

--- recop.it/src/scripts/preparation/join_files.py
+++ recop.it/src/scripts/preparation/join_files.py
@@ -16,3 +16,8 @@ def get_files(home: str) -> types.GeneratorType[str]:
 def read_csv(address):
     """Returns a dataframe from the CSV file read"""
     return pd.read_csv(address)
+
+
+def stack_df(df1, df2):
+    """Stacks two dataframes on top of each other, vertically"""
+    return pd.concat([df1, df2])
