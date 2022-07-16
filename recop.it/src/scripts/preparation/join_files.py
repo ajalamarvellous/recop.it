feaecd898719c612d017fb8042fd6b6fa2b33131
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-07-16 06:38:52 +0100

    Edit error on global location address

--- recop.it/src/scripts/preparation/join_files.py
+++ recop.it/src/scripts/preparation/join_files.py
@@ -1,23 +1,22 @@
 import logging
-import types
 from pathlib import Path
 
 import pandas as pd
 
-home = Path.cwd().parents[2]
+LOCATION = Path(__file__).resolve().parents[3].joinpath("data", "processed")
 
 logging.basicConfig(
     format="%(asctime)s [%(levelname)s]\
             %(funcName): %(message)s",
-    levelname=logging.DEBUG,
+    level=logging.DEBUG,
 )
 logger = logging.getLogger(__name__)
 
 
-def get_files(home: str) -> types.GeneratorType[str]:
+def get_files(LOCATION):
     """Returns a iterator for the address of all the files in it"""
-    assert home.exists()
-    files = home.joinpath("data", "processed").iterdir()
+    assert LOCATION.exists()
+    files = LOCATION.iterdir()
     return files
 
 
@@ -38,5 +37,5 @@ def drop_cols(df, columns):
 
 def save_file(df, name):
     """Saves the file with the given name"""
-    address = home.joinpath("data", "processed", name)
+    address = LOCATION.joinpath(name)
     df.to_csv(address)
